0b2263c871d3e818382c104ad035ae7851221d1a
F. Permadi <permadi@permadi.com>
2018-07-13 14:26:49 -0700

    Added release notes

--- demo/1/sample1.js
+++ demo/1/sample1.js
@@ -1,3 +1,41 @@
+/**********************************************
+Raycasting implementation in Javascript.
+First Demo
+Source: https://github.com/permadi-com/ray-cast/tree/master/demo/1
+
+See it in action: https://permadi.com/tutorial/raycast/demo/1/
+
+What's on this demo:
+Wall finding
+Generating lookup tables
+Fishbowl / distortion corrections
+Simple flat wall shading
+Rendering of simple (static) ground and sky
+Movement handling
+
+---------------
+
+License: MIT (https://opensource.org/licenses/MIT)
+
+Copyright 2015-2018 F. Permadi
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this 
+software and associated documentation files (the "Software"), 
+to deal in the Software without restriction, 
+including without limitation the rights to use, copy, modify, merge, publish, 
+distribute, sublicense, and/or sell copies of the Software, and to permit persons to 
+whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all 
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+***********************************************/
 function GameWindow(canvas) {
     this.width = canvas.width;
     this.height = canvas.height;  
